130fb744ad691d5723374654877f36583efceb30
mohamed.amin <mohamedamin1421@gmail.com>
2025-08-09 17:27:23 +0300

    feat: add sla controller

---
+++ SLA Deadline Calculator/API/Service Level Agreement (SLA)/Controllers/SLAController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SLA.Application;
+
+namespace SLA_.API;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SLAController : ControllerBase
+{
+    private readonly ICalculateSlaService _slaService;
+
+    public SLAController(ICalculateSlaService slaService)
+    {
+        _slaService = slaService;
+    }
+
+    [HttpGet("calculate-deadline")]
+    public async Task<ComplaintResponseDto> CalculateDeadline(
+        ComplaintRequestDto request,
+        CancellationToken cancellationToken
+    )
+    {
+        var result = await _slaService.CalculateResolutionDeadlineAsync(
+            request.Priority,
+            request.CaptureDateTime,
+            cancellationToken
+        );
+
+        return new ComplaintResponseDto(result);
+    }
+}
